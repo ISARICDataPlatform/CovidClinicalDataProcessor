853df26e7b1e35cf2028c1c4ad68fe062e65f9b0
twoseventwo <matthew.hall@bdi.ox.ac.uk>
2020-08-19 12:50:43 +0100

    Removed library calls

--- R/SimulatedDataFunctions.R
+++ R/SimulatedDataFunctions.R
@@ -12,11 +12,10 @@
 
 
 synthesise <- function(data, var_identifier = "usubjid"){
-  library(dplyr);library(lubridate);library(stats);library(purrr);library(tidyselect)
   
   # Classify variables--------------
   if(is.null(var_identifier)==F){var_identifer <- data %>%
-    dplyr::select(tidyselect::any_of(c(var_identifier))) %>% 
+    select(tidyselect::any_of(c(var_identifier))) %>% 
     names()}
   
   var_date <- data %>% select_if(lubridate::is.Date) %>% names()
@@ -28,25 +27,25 @@ synthesise <- function(data, var_identifier = "usubjid"){
   synth <- data %>%
     
     # Adjust all dates by -60 to 100 days
-    dplyr::group_by(usubjid) %>%
-    dplyr::mutate(date_shift = ceiling(stats::runif(1, -60, 100))) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate_at(vars(all_of(var_date)), ~ lubridate::as_date(ifelse(is.na(.)==T, NA, lubridate::as_date(.)+date_shift))) %>%
+    group_by(usubjid) %>%
+    mutate(date_shift = ceiling(runif(1, -60, 100))) %>%
+    ungroup() %>%
+    mutate_at(vars(all_of(var_date)), ~ as_date(ifelse(is.na(.)==T, NA, lubridate::as_date(.)+date_shift))) %>%
     
     # Randomly generate numbers for each variable within current range.
-    dplyr::mutate_at(vars(all_of(var_numeric)), function(x){ceiling(stats::runif(x, min(x, na.rm = T), max(x, na.rm = T)))}) %>%
+    mutate_at(vars(all_of(var_numeric)), function(x){ceiling(runif(x, min(x, na.rm = T), max(x, na.rm = T)))}) %>%
     
     # randomly change values (within a max of 10 options for each var) if not NA
-    dplyr::mutate_at(vars(all_of(var_other)),
+    mutate_at(vars(all_of(var_other)),
                      function(x){purrr::map2_chr(.x = as.character(x),
                                                  .y = sample(rep(names(as.character(x) %>% table() %>% sort(decreasing = T) %>% head(10)), length(x)))[1:length(x)],
                                                  function(.x, .y){ifelse(is.na(as.character(.x))==F, sample(.y, replace=T)[1], NA)})}) %>%
     
     # randomly order the unique identifer, then convert to a number
-    dplyr::mutate_at(vars(all_of(var_identifer)),
+    mutate_at(vars(all_of(var_identifer)),
                      function(x){factor(x, levels = x %>% sample()) %>% as.numeric}) %>%
-    dplyr::arrange(usubjid) %>%
-    dplyr::select(-date_shift)
+    arrange(usubjid) %>%
+    select(-date_shift)
   
   
   return(synth)}
