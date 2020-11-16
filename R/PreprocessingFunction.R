13e28aac9f2f339a72df2a2e7c4df1f350a14920
martinaescher <72395428+martinaescher@users.noreply.github.com>
2020-11-16 23:28:43 +0100

    prepocessing

---
+++ R/PreprocessingFunction.R
@@ -0,0 +1,104 @@
+
+#' Preprocessing step for all aggregations. Currently: remaps outcome to death, discharge or NA, cuts age into 5-year age groups, and adds a year-epiweek column
+#' @param input.tbl Input tibble (output of \code{process.all.data})
+#' @import dtplyr dplyr purrr lubridate tibble
+#' @importFrom glue glue
+#' @return A \code{tibble} intended for input into other aggregation functions (e.g. \code{age.pyramid.prep})
+#' @export data.preprocessing
+
+data.preprocessing <- function(input.tbl){
+  input.tbl %>%
+    lazy_dt(immutable = TRUE) %>%
+    mutate(date_hoin_latest=case_when(is.na(date_ho_latest) ~ date_in_latest,
+                                      is.na(date_in_latest) ~ date_ho_latest,
+                                      date_ho_latest>date_in_latest ~ date_ho_latest,
+                                      date_ho_latest<=date_in_latest ~ date_in_latest))%>%
+    mutate(date_latest)
+    mutate(outcome.3 = map2_chr(outcome, date_outcome, outcome.remap)) %>%
+    select(-outcome) %>%
+    rename(slider_outcome = outcome.3) %>%
+    mutate(agegp10 = cut(age, right = FALSE, breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 120))) %>%
+    mutate(date_admit=as_date(date_admit))%>%
+    mutate(calendar.year.admit = year(date_admit)) %>%
+    mutate(calendar.month.admit = month(date_admit)) %>%
+    mutate(slider_monthyear = map2_chr(calendar.year.admit, calendar.month.admit, month.year.mapper)) %>%
+    mutate(year.admit = map_dbl(date_admit, epiweek.year)) %>%
+    mutate(epiweek.admit = epiweek(date_admit)) %>%
+    mutate(year.epiweek.admit=paste0(year.admit,"-", epiweek.admit))%>%
+    #mutate(year.epiweek.admit = glue("{year.admit}-{epiweek.admit}", .envir = .SD)) %>%
+    mutate(year.epiweek.admit = replace(year.epiweek.admit, year.epiweek.admit == "NA-NA", NA)) %>%
+    mutate(lower.age.bound  = map_dbl(agegp10, extract.age.boundaries, TRUE)) %>%
+    mutate(upper.age.bound  = map_dbl(agegp10, extract.age.boundaries, FALSE)) %>%
+    mutate(slider_agegp10 = fct_relabel(agegp10, prettify.age.labels)) %>%
+    select(-agegp10) %>%
+    rename(slider_icu_ever = ever_icu) %>%
+    rename(slider_country = country) %>%
+    rename(slider_sex = sex) %>%
+    as_tibble()
+}
+
+#' @keywords internal
+#' @export outcome.remap
+outcome.remap <- function(oc, od){
+  if(is.na(od) & is.na(oc)){
+    "censored"
+  } else {
+    out <- case_when(is.na(oc) ~ NA_character_,
+                     oc == "Death" ~ "death",
+                     oc == "Discharge" ~ "discharge",
+                     oc == "Ongoing care" ~ "Ongoing care",
+                     oc == "Transferred" ~ "transferred"
+    )
+    
+  }
+}
+
+#' @keywords internal
+#' @export prettify.age.labels
+prettify.age.labels <- function(a){
+  temp <- substr(a, 2, nchar(a) - 1)
+  newlabels <- map_chr(temp, function(x) {
+    components <- as.numeric(str_split_fixed(x, ",", Inf))
+    components[2] <- components[2] - 1
+    paste(components, collapse = "-")
+  })
+  str_replace(newlabels, "90-119", "90+")
+}
+
+#' @keywords internal
+#' @export extract.age.boundaries
+extract.age.boundaries <- function(agestring, lower = TRUE){
+  agestring <- as.character(agestring)
+  temp <- substr(agestring, 2, nchar(agestring)-1)
+  if(lower){
+    as.numeric(str_split_fixed(temp, ",", Inf)[1])
+  } else {
+    as.numeric(str_split_fixed(temp, ",", Inf)[2]) - 1
+  }
+}
+
+#' @keywords internal
+#' @export epiweek.year
+epiweek.year <- function(date){
+  if(is.na(date)){
+    return(NA)
+  }
+  if(year(date)==2019 & date > ymd("2019-12-28")){
+    2020
+  } else {
+    year(date)
+  }
+}
+
+#' @keywords internal
+#' @export month.year.mapper
+month.year.mapper <- function(y,m){
+  if(any(is.na(c(y,m)))){
+    NA
+  } else if(m<10){
+    glue("0{m}-{y}")
+  } else {
+    glue("{m}-{y}")
+  }
+}
+
