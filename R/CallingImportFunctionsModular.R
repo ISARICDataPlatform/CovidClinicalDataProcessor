0f2bfa8aa254cec68de3dbe40e656d9e4eaab995
martina escher <martina.escher@gmail.com>
2021-05-13 22:27:48 +0200

    new changes

--- R/CallingImportFunctionsModular.R
+++ R/CallingImportFunctionsModular.R
@@ -1,7 +1,6 @@
 #libraries
 
-
-
+library("ff")
 library(stringr)
 library(plyr)
 library(tidyverse)
@@ -13,7 +12,10 @@ library(data.table)
 library(tidyfast)
 library(glue)
 library(lubridate)
-
+library(readr)
+library(janitor)
+library(tools)
+library(stringy)
 
 
 
@@ -24,9 +26,9 @@ library(lubridate)
 folder <- "C:/Users/baruj003/Desktop/21/working_R/oxford/CovidClinicalDataProcessor/data"
 
 setwd(folder)
-memory.limit(size=80000)
+memory.limit(size=160000)
 
-folder <- "C:/Users/marti/OneDrive/Documents/ISARIC/data/2021-02-15"
+folder <- "C:/Users/marti/OneDrive/Documents/ISARIC/data/2021-04-07"
 setwd(folder)
 
 #'Importing csv files
@@ -45,28 +47,47 @@ colnames(lb) <- tolower(colnames(lb))
 ds<-read.csv("DS.csv")
 colnames(ds) <- tolower(colnames(ds))
 
-
-sa<-
+sa7<-
   read_csv(
-    "sa_all_isaric.csv",
+   "sa_1_saterm_no_quotes.csv",
     col_names = TRUE,
     col_types = NULL,
     locale = default_locale(),
     na = c("", "NA"),
     quoted_na = TRUE,
-    quote = "\"",
+    #quote = quote,
     comment = "",
     trim_ws = TRUE,
     skip = 0,
     n_max = Inf,
-    guess_max = min(8000000, 20000000),
+    guess_max = min(8000000, 12000000000),
     progress = show_progress(),
     skip_empty_rows = TRUE
   )
+
 colnames(sa) <- tolower(colnames(sa))
+save(sa,file="sa.rda")
+
+
+
+
+int4 <- read.csv.ffdf(file="in.csv", header=TRUE, VERBOSE=TRUE, quote = '"',
+                      first.rows=1000, next.rows=5000000, 
+                      colClasses = c(rep("factor", 66), rep("NULL", 66)), na = '')
+int4<-as.data.frame(int4)
+colnames(int4) <- tolower(colnames(int4))
+save(int,file="int.rda")
+
+
+
+
+save(int4,file="int.rda")
+
+
+int<-int_2
 INT<-
   read_csv(
-    "in_all_isaric.csv",
+    "IN.csv",
     col_names = TRUE,
     col_types = NULL,
     locale = default_locale(),
@@ -77,14 +98,15 @@ INT<-
     trim_ws = TRUE,
     skip = 0,
     n_max = Inf,
-    guess_max = min(8000000, 20000000),
+    guess_max = min(8000000, 25000000),
     progress = show_progress(),
     skip_empty_rows = TRUE
   )
-colnames(INT) <- tolower(colnames(INT))
+colnames(int) <- tolower(colnames(int))
+save(int,file="int.rda")
 
 ###set date pull
-date_pull<-as_date("2021-02-15") 
+date_pull<-as_date("2021-04-07") 
 
 ###calling data import functions
 
@@ -93,48 +115,56 @@ save(imp_dm, file = "imp_dm.rda")
 
 
 imp_mb <- import.microb.data(mb, dtplyr.step = FALSE)
-
 save(imp_mb, file = "imp_mb.rda")
 
 imp_rp <- process.pregnancy.data(rp, dtplyr.step = FALSE)
 save(imp_rp, file = "imp_rp.rda")
 
-imp_int<-process.treatment.data(int, dtplyr.step = FALSE)
+load("int.rda")
+int<-int4
+
+incl_studyid<-unique(as.character(int$studyid))  
+
+  
+int<-int4%>%
+  filter(studyid=="CVZXZMV")%>%
+  slice_head(n = 5257525)
+  
+  
+imp_int_CVZXZMV_head<-process.treatment.data(int, dtplyr.step = FALSE)
+
+int<-int4%>%
+  filter(studyid=="CVZXZMV")%>%
+  slice_tail(n = 5257525)
+
+
+imp_int_CVZXZMV_tail<-process.treatment.data(int, dtplyr.step = FALSE)
+
+int<-int4%>%
+  filter(studyid=="CVCCPUK")
+imp_int_CVCCPUK<-process.treatment.data(int, dtplyr.step = FALSE)
+
+
+int<-int4%>%
+  filter(studyid!="CVCCPUK"&studyid!="CVZXZMV")
+imp_int_rest<-process.treatment.data(int, dtplyr.step = FALSE)
+
+imp_int<-imp_int_rest%>%
+    rbind(imp_int_CVCCPUK)%>%
+    rbind(imp_int_CVZXZMV_tail)%>%
+    rbind(imp_int_CVZXZMV_head)
 save(imp_int, file = "imp_int.rda")
 
-imp_treat<-process.common.treatment.data(imp_int, minimum=10, dtplyr.step = FALSE)
+load(file="imp_int.rda")
+
+imp_treat<-process.common.treatment.data(imp_int, minimum=100, dtplyr.step = FALSE)
 save(imp_treat, file = "imp_treat.rda")
 
 imp_icu<- process.ICU.data(ho, dtplyr.step = FALSE)
 save(imp_icu, file = "imp_icu.rda")
 
-icu_ever<-imp_icu%>%
-  filter(ever_icu==TRUE)%>%
-  filter(!is.na(icu_in))
-
-imp_treat_icu<-imp_int%>%
-  filter(!is.na(indtc))%>%
-  filter(indtc>= "2020-01-01"|indtc<date_pull)%>%
-  left_join(icu_ever,by = c("usubjid"))%>%
-  mutate(int_icu=case_when(indtc>=icu_in ~ TRUE, 
-                           TRUE ~ FALSE))%>%
-  filter(int_icu==TRUE)%>%
-  arrange(desc(inoccur))%>%
-  distinct(usubjid, treatment, .keep_all =T)%>%
-  group_by(treatment) %>% 
-  arrange(desc(inoccur))%>%
-  mutate(n = sum(!is.na(inoccur))) %>%
-  filter(n >= eval(10)) %>%
-  ungroup()%>%
-  mutate(treatment = glue("icu_treat_{treatment}", treatment = treatment)) %>%
-  as.data.table() %>%
-  dt_pivot_wider(id_cols = usubjid, names_from = treatment,  values_from = inoccur)
-save(imp_treat_icu, file = "imp_treat_icu.rda") 
-
-
-imp_ventilation <- process.IMV.NIV.data(imp_int, dtplyr.step = FALSE)
-save(imp_ventilation, file = "imp_ventilation.rda") 
-
+imp_treat_icu<-process.treatment.icu.data(imp_int, imp_icu, minimum=100,dtplyr.step = FALSE)
+save(imp_treat_icu, file = "imp_treat_icu.rda")
 
 imp_vs<- process.vital.sign.data(vs, dtplyr.step = FALSE)
 save(imp_vs, file = "imp_vs.rda")
@@ -144,31 +174,68 @@ save(imp_lb, file = "imp_lb.rda")
 
 imp_ds <-process.outcome.data(ds, dtplyr.step = FALSE)
 save(imp_ds, file = "imp_ds.rda")
+tab<-tabyl(imp_ds$outcome)
+
+
+
+load("sa.rda")
+sab<-sa
+
+sa<-sab%>%
+  filter(studyid=="CVZXZMV")
+imp_sa_CVZXZMV<-import.symptom.and.comorbidity.data(sa, dtplyr.step = FALSE)
+
+
+sa<-sab%>%
+  filter(studyid=="CVCCPUK")
+imp_sa_CVCCPUK<-import.symptom.and.comorbidity.data(sa, dtplyr.step = FALSE)
+
+
+sa<-sab%>%
+  filter(studyid!="CVCCPUK"&studyid!="CVZXZMV")
+imp_sa_rest<-import.symptom.and.comorbidity.data(sa, dtplyr.step = FALSE)
+
+imp_sa<-imp_sa_rest%>%
+  rbind(imp_sa_CVCCPUK)%>%
+  rbind(imp_sa_CVZXZMV)
+
+imp_sa_com<-imp_sa
+save(imp_sa_com, file = "imp_sa_com.rda")
+
+
 
 imp_sa<-import.symptom.and.comorbidity.data(sa, dtplyr.step = FALSE)
 tabyl(imp_sa$saterm)
 save(imp_sa, file = "imp_sa.rda")
 
-
-imp_comorb<-process.comorbidity.data(imp_sa, minimum=100, dtplyr.step = FALSE)
+load(file="imp_sa_com.rda")
+imp_comorb<-process.comorbidity.data(imp_sa_com, minimum=100, dtplyr.step = FALSE)
 save(imp_comorb, file = "imp_comorb.rda")
 
-imp_symptom<-process.symptom.data(imp_sa, minimum=100, dtplyr.step = FALSE)
+imp_symptom<-process.symptom.data(imp_sa_sa, minimum=100, dtplyr.step = FALSE)
 save(imp_symptom, file = "imp_symptom.rda")
 
-clinic_diagn<-process.covid.clinic.diagn(imp_sa,dtplyr.step = FALSE)
-save(clinic_diagn, file = "clinic_diagn.rda")
 
+
+load(file="imp_dm.rda")
+load(file="imp_mb.rda")
+load(file="imp_comorb.rda")
+load(file="imp_rp.rda")
+load(file="imp_symptom.rda")
+load(file="imp_treat.rda")
+load(file="imp_icu.rda")
+load(file="imp_treat_icu.rda")
+load(file="imp_lb.rda")
+load(file="imp_vs.rda")
+load(file="imp_ds.rda")
 
 #########joining all data
 import.tbl<-imp_dm%>%
   left_join(imp_mb, by = c("usubjid"))%>%
-  left_join(clinic_diagn, by = c("usubjid"))%>%
   left_join(imp_comorb, by = c("usubjid"))%>%
   left_join(imp_rp, by = c("usubjid"))%>%
   left_join(imp_symptom, by = c("usubjid"))%>%
   left_join(imp_treat, by = c("usubjid"))%>%
-  #left_join(imp_ventilation, by = c("usubjid"))%>%
   left_join(imp_icu, by = c("usubjid"))%>%
   left_join(imp_treat_icu, by = c("usubjid"))%>%
   left_join(imp_lb, by = c("usubjid"))%>%
